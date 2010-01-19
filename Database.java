92a7cace67c1ef1c78c50b88382dc0d3238836cd
Andy Brown <adb1413@rit.edu>
2010-01-19 17:06:13 -0500

    More flexibility in Database for statements.

--- Database.java
+++ Database.java
@@ -18,21 +18,82 @@ import java.sql.Statement;
  */
 public class Database {
 	
-	
 	private static Connection connection;
 	
+	
+	/**
+	 * Gets a new statement from the static connection.
+	 * 
+	 * @throws SQLException when a statement can't be made.
+	 */
+	public static Statement createStatement()
+	                                        throws SQLException {
+		
+		// Return statement
+		initConnection();
+		return connection.createStatement();
+	}
+	
+	
+	/**
+	 * Executes a query on the database.
+	 */
+	public static ResultSet executeQuery(String sql) 
+	                                     throws SQLException {
+		
+		Statement statement;
+		
+		// Pass statement
+		initConnection();
+		statement = connection.createStatement();
+		return statement.executeQuery(sql);
+	}
+	
+	
+	/**
+	 * Updates, inserts, or deletes something in the database.
+	 */
+	public static int executeUpdate(String sql)
+	                                throws SQLException {
+		
+		Statement statement;
+		
+		// Pass statement
+		initConnection();
+		statement = connection.createStatement();
+		return statement.executeUpdate(sql);
+	}
+	
+	
+	/**
+	 * Gets the connection to the database.
+	 */
+	public static Connection getConnection()
+	                                       throws SQLException {
+		
+		// Check connection
+		initConnection();
+		return connection;
+	}
 	
 	
 	/**
 	 * Initializes the connection to the database.
+	 * 
+	 * @throws SQLException when there's a problem connecting.
 	 */
-	private static void initConnection() {
+	private static void initConnection() 
+	                                   throws SQLException {
 		
 		String host, name, user, password, url;
 		
+		// Check if already connected
+		if (connection != null)
+			return;
+		
 		try {
 			
-			// Find credentials from XML file
+			// Get credentials from configuration
 			host = Configuration.getOption("database", "host");
 			name = Configuration.getOption("database", "name");
 			user = Configuration.getOption("database", "user");
@@ -41,39 +102,14 @@ public class Database {
 			// Make connection
 			url = String.format("jdbc:mysql://%s/%s", host, name);
 			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager.getConnection(url, user, password);
-		}
-		catch (IOException e) {
-			System.err.println(e.getMessage());
-			System.exit(1);
+			connection =  DriverManager.getConnection(url, user, password);
+			
+		} catch (IOException e) {
+			throw new SQLException(e.getMessage());
 		} catch (ClassNotFoundException e) {
-			System.err.println("[Database] Could not find MySQL driver.");
-			System.exit(1);
-		} catch (SQLException e) {
-			System.err.println("[Database] No connection to database.");
-			System.exit(1);
+			throw new SQLException(e.getMessage());
 		}
 	}
-	
-	
-	
-	/**
-	 * Executes a query on the database.
-	 */
-	public static ResultSet executeQuery(String query) 
-	                                     throws SQLException {
-		
-		Statement statement;
-		
-		// Check connection
-		if (connection == null)
-			initConnection();
-		
-		// Pass statement
-		statement = connection.createStatement();
-		return statement.executeQuery(query);
-	}
-	
 	
 	
 	/**
@@ -92,12 +128,15 @@ public class Database {
 		
 		// Test
 		try {
-			rs = Database.executeQuery("SELECT * FROM address");
+			rs = Database.executeQuery("SELECT * FROM sponsor");
 			while (rs.next()) {
+				System.out.println(rs.getString("name"));
 				System.out.println(rs.getString("street"));
-				System.out.println(rs.getString("city"));
-				System.out.println(rs.getString("state"));
-				System.out.println(rs.getString("zip"));
+				System.out.printf("%s, %s %s\n",
+				                  rs.getString("city"),
+				                  rs.getString("state"),
+				                  rs.getString("zip"));
+				System.out.println(rs.getString("phone"));
 				System.out.println();
 			}
 		} catch (SQLException e) {

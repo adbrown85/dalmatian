61d41129f2773e4054a5caefa7f7eb4393525298
Andy Brown <adb1413@rit.edu>
2010-01-21 06:57:02 -0500

    Spot dialogs improved. (SpotDialog, SpotSelector).
    SpotSelector can now get spots in a filtered manner by using
    three combo boxes: Sponsor, Title, Year.  Selecting a sponsor
    changes the titles available, and selecting a title changes the
    years available.  Selecting a year fills in the fields for
    filename and description.
    
    SpotSelector also has a nifty Play/Stop feature!
    
    Lastly, SpotDialog has better dialogs for confirmations and
    errors.

--- SpotDialog.java
+++ SpotDialog.java
@@ -17,12 +17,7 @@ import javax.swing.*;
 /**
  * Dialog for adding a Spot.
  */
-public class SpotDialog extends JDialog
-                        implements ActionListener {
-	
-	JPanel contentPane;
-	JPanel buttonPanel, inputPanel;
-	TreeMap<String,JComponent> inputs;
+public class SpotDialog extends InputDialog {
 	
 	
 	/**
@@ -40,241 +35,103 @@ public class SpotDialog extends JDialog
 	
 	public void actionPerformed(ActionEvent event) {
 		
-		String command = event.getActionCommand();
+		String command;
 		
 		// Handle command
+		command = event.getActionCommand();
 		if (command.equals("Add")) {
 			handleAdd();
-		} else if (command.equals("Browse")) {
-			handleBrowse();
 		} else if (command.equals("Cancel")) {
-			reset();
-			setVisible(false);
+			handleCancel();
 		} else if (command.equals("Reset")) {
-			reset();
+			handleReset();
 		}
 	}
 	
 	
 	private void handleAdd() {
 		
+		int type;
 		Spot spot;
+		String message, title;
 		
-		// Get text fields
-		spot = new Spot();
-		spot.setName(((JTextField)inputs.get("Name")).getText());
-		spot.setFilename(((JTextField)inputs.get("Filename")).getText());
-		spot.setDescription(((JTextArea)inputs.get("Description")).getText());
-		spot.setSponsor((String)((JComboBox)inputs.get("Sponsor")).getSelectedItem());
-		spot.setYear((Integer)((JComboBox)inputs.get("Year")).getSelectedItem());
-		
-		// Try insert
+		// Get and insert
 		try {
+			spot = getSpot();
 			spot.insert();
-			reset();
+			message = "Inserted spot.";
+			JOptionPane.showMessageDialog(this, message);
+			clear();
 			setVisible(false);
 		} catch (SQLException e) {
-			System.err.println("[SpotDialog] Could not insert new spot.");
+			System.err.println("[SpotDialog] " + e.getMessage());
+			title = "Error";
+			type = JOptionPane.ERROR_MESSAGE;
+			message = "Could not insert spot.\n\n" + e.getMessage();
+			JOptionPane.showMessageDialog(this, message, title, type);
 		}
 	}
 	
 	
-	private void handleBrowse() {
+	private void handleCancel() {
 		
-		File file;
-		int result;
-		JFileChooser fileChooser;
-		
-		// Get filename
-		fileChooser = new JFileChooser();
-		result = fileChooser.showOpenDialog(this);
-		if (result == JFileChooser.APPROVE_OPTION) {
-			file = fileChooser.getSelectedFile();
-			((JTextField)(inputs.get("Filename"))).setText(file.getPath());
-		}
+		clear();
+		setVisible(false);
 	}
 	
 	
-	private void reset() {
+	private void handleReset() {
 		
-		JComponent component;
+		clear();
+	}
+	
+	
+	public Spot getSpot() {
 		
-		// Clear text
-		((JTextField)inputs.get("Name")).setText(null);
-		((JTextField)inputs.get("Filename")).setText(null);
-		((JTextArea)inputs.get("Description")).setText(null);
+		Spot spot;
 		
-		// Reset combo boxes
-		((JComboBox)inputs.get("Sponsor")).setSelectedIndex(0);
-		((JComboBox)inputs.get("Year")).setSelectedIndex(0);
+		// Get inputs
+		spot = new Spot();
+		spot.setSponsor((String)getItemFrom("Sponsor"));
+		spot.setTitle(getTextFrom("Title"));
+		spot.setYear((Integer)getItemFrom("Year"));
+		spot.setFilename(getFilenameFrom("Filename"));
+		spot.setDescription(getTextFrom("Description"));
+		return spot;
 	}
 	
 	
 	private void init()
 	                  throws SQLException {
 		
-		// Initialize characteristics
+		// Miscellaneous
 		setResizable(false);
 		
-		// Initialize panels
-		initContentPane();
-		initInputPanel();
-		initButtonPanel();
+		// Panels
+		initInputs();
+		initButtons();
 	}
 	
 	
-	private void initButtonPanel() {
+	private void initButtons() {
 		
-		JButton button;
 		String[] names={"Add","Cancel","Reset"};
-		
-		// Create and add panel
-		buttonPanel = new JPanel();
-		contentPane.add(buttonPanel);
 		
 		// Add buttons
 		for (String name : names) {
-			button = new JButton(name);
-			button.addActionListener(this);
-			button.setActionCommand(name);
-			buttonPanel.add(button);
+			buttonPanel.addButton(name);
 		}
 	}
 	
 	
-	private void initContentPane() {
+	private void initInputs() 
+	                        throws SQLException {
 		
-		// Change content pane layout
-		contentPane = new JPanel();
-		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.PAGE_AXIS));
-		contentPane.setBorder(BorderFactory.createEmptyBorder(4,4,4,4));
-		setContentPane(contentPane);
-	}
-	
-	
-	private void initInput(JComponent component,
-	                       int x,
-	                       int y) {
-		
-		GridBagConstraints gbc;
-		
-		// Setup constraints and add component
-		gbc = new GridBagConstraints();
-		gbc.anchor = GridBagConstraints.LINE_START;
-		gbc.gridx = x;
-		gbc.gridy = y;
-		gbc.insets = new Insets(2, 4, 2, 4);
-		inputPanel.add(component, gbc);
-	}
-	
-	
-	private void initInputForSponsor()
-	                                 throws SQLException {
-		
-		JComboBox comboBox;
-		
-		// Add label and combo box
-		initInput(new JLabel("Sponsor"), 0, 0);
-		comboBox = new JComboBox(Sponsor.retrieveAllNames());
-		initInput(comboBox, 1, 0);
-		inputs.put("Sponsor", comboBox);
-	}
-	
-	
-	private void initInputForName() {
-		
-		JTextField textField;
-		
-		// Add label and text field
-		initInput(new JLabel("Name"), 0, 1);
-		textField = new JTextField(20);
-		initInput(textField, 1, 1);
-		inputs.put("Name", textField);
-	}
-	
-	
-	private void initInputForYear() {
-		
-		JComboBox comboBox;
-		Vector<Integer> years;
-		
-		// Add label and combo box
-		initInput(new JLabel("Year"), 0, 2);
-		years = new Vector<Integer>();
-		for (int i=2010; i<2020; ++i) {
-			years.add(new Integer(i));
-		}
-		comboBox = new JComboBox(years);
-		initInput(comboBox, 1, 2);
-		inputs.put("Year", comboBox);
-	}
-	
-	
-	private void initInputForFilename() {
-		
-		JButton button;
-		JTextField textField;
-		
-		// Add label, text field, and button
-		initInput(new JLabel("Filename"), 0, 3);
-		textField = new JTextField(20);
-		initInput(textField, 1, 3);
-		inputs.put("Filename", textField);
-		button = new JButton("Browse");
-		button.addActionListener(this);
-		button.setActionCommand("Browse");
-		initInput(button, 2, 3);
-	}
-	
-	
-	private void initInputForDescription() {
-		
-		GridBagConstraints gbc;
-		JLabel label;
-		JTextArea textArea;
-		JScrollPane scrollPane;
-		
-		// Add label
-		label = new JLabel("Description");
-		gbc = new GridBagConstraints();
-		gbc.anchor = GridBagConstraints.FIRST_LINE_START;
-		gbc.gridx = 0;
-		gbc.gridy = 4;
-		gbc.insets = new Insets(2, 4, 2, 4);
-		inputPanel.add(label, gbc);
-		
-		// Add text area with scroll pane
-		textArea = new JTextArea(4, 28);
-		textArea.setLineWrap(true);
-		textArea.setWrapStyleWord(true);
-		scrollPane = new JScrollPane(textArea);
-		inputs.put("Description", textArea);
-		gbc = new GridBagConstraints();
-		gbc.anchor = GridBagConstraints.FIRST_LINE_START;
-		gbc.gridx = 1;
-		gbc.gridy = 4;
-		gbc.gridwidth = 2;
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		gbc.insets = new Insets(2, 4, 2, 4);
-		inputPanel.add(scrollPane, gbc);
-	}
-	
-	
-	private void initInputPanel()
-	                            throws SQLException {
-		
-		// Create and add to content pane
-		inputPanel = new JPanel(new GridBagLayout());
-		inputPanel.setBorder(BorderFactory.createTitledBorder("Input"));
-		contentPane.add(inputPanel);
-		
-		// Add labels and fields
-		inputs = new TreeMap<String,JComponent>();
-		initInputForSponsor();
-		initInputForName();
-		initInputForYear();
-		initInputForFilename();
-		initInputForDescription();
+		inputPanel.addInput("Sponsor", new JComboBox(Sponsor.getAllNames()));
+		inputPanel.addInput("Title", new JTextField(20));
+		inputPanel.addInput("Year", new JComboBox(Year.getAllYears()));
+		inputPanel.addInput("Filename", new FilenameInput(24, this));
+		inputPanel.addInput("Description", new JTextArea(4,30));
 	}
 	
 	
---
+++ SpotSelector.java
@@ -0,0 +1,345 @@
+/*
+ * SpotSelector.java
+ * 
+ * Author
+ *     Andrew Brown <andrew@andrewdbrown.com>
+ */
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.sql.SQLException;
+import java.util.TreeMap;
+import java.util.Vector;
+import javax.swing.*;
+
+
+
+/**
+ * Dialog for adding a Spot.
+ */
+public class SpotSelector extends InputDialog {
+	
+	private static Retriever retriever=null;
+	private boolean pausedEvents=false;
+	private boolean playingAudio=false;
+	private AudioPlayer audioPlayer=null;
+	
+	
+	/**
+	 * Creates a new %SpotSelector.
+	 */
+	public SpotSelector(JFrame frame,
+	                    String title)
+	                    throws SQLException {
+		
+		// Initialize
+		super(frame, title);
+		init();
+	}
+	
+	
+	public void actionPerformed(ActionEvent event) {
+		
+		String command;
+		
+		// Check pause
+		if (pausedEvents) {
+			return;
+		}
+		
+		// Handle command
+		command = event.getActionCommand();
+		if (command.equals("Sponsor")) {
+			handleSponsor();
+		} else if (command.equals("Title")) {
+			handleTitle();
+		} else if (command.equals("Year")) {
+			handleYear();
+		} else if (command.equals("Play")) {
+			handlePlay();
+		} else if (command.equals("Stop")) {
+			handleStop();
+		} else if (command.equals("Refresh")) {
+			handleRefresh();
+		} else if (command.equals("Select")) {
+			handleSelect();
+		} else if (command.equals("Cancel")) {
+			handleCancel();
+		} else if (command.equals("AUDIOPLAYER_PLAYING")) {
+			handleAudioPlayerPlaying();
+		} else if (command.equals("AUDIOPLAYER_STOPPED")) {
+			handleAudioPlayerStopped();
+		}
+	}
+	
+	
+	public Vector<String> getSponsorsWithSpots()
+	                                           throws SQLException {
+		
+		return retriever.getSponsorsWithSpots();
+	}
+	
+	
+	public Vector<String> getTitlesForSponsor()
+	                                          throws SQLException {
+		
+		return retriever.getTitlesForSponsor((String)getItemFrom("Sponsor"));
+	}
+	
+	
+	public Vector<Integer> getYearsForSponsorTitle()
+	                                               throws SQLException {
+		
+		return retriever.getYearsForSponsorTitle((String)getItemFrom("Sponsor"),
+		                                         (String)getItemFrom("Title"));
+	}
+	
+	
+	private void handleAudioPlayerPlaying() {
+		
+		playingAudio = true;
+	}
+	
+	
+	private void handleAudioPlayerStopped() {
+		
+		playingAudio = false;
+	}
+	
+	
+	private void handleCancel() {
+		
+		pauseEvents();
+		clear();
+		restartEvents();
+		setVisible(false);
+	}
+	
+	
+	private void handlePlay() {
+		
+		if (playingAudio)
+			return;
+		
+		audioPlayer = new AudioPlayer(getTextFrom("Filename"));
+		audioPlayer.addActionListener(this);
+		audioPlayer.start();
+	}
+	
+	
+	private void handleRefresh() {
+		
+		JComboBox sponsorCombo;
+		Vector<String> sponsors;
+		
+		try {
+			pauseEvents();
+			sponsorCombo = (JComboBox)getInput("Sponsor");
+			sponsorCombo.removeAllItems();
+			restartEvents();
+			sponsors = getSponsorsWithSpots();
+			for (String sponsor : sponsors) {
+				sponsorCombo.addItem(sponsor);
+			}
+		} catch (SQLException e) {
+		}
+	}
+	
+	
+	private void handleSelect() {
+		
+		try {
+			getSpot().print();
+		} catch (SQLException e) {
+			System.err.println("[SpotSelector] Unexpected error in getSpot.");
+		}
+	}
+	
+	
+	private void handleSponsor() {
+		
+		JComboBox titleCombo;
+		Vector<String> titles;
+		
+		try {
+			pauseEvents();
+			titleCombo = (JComboBox)getInput("Title");
+			titleCombo.removeAllItems();
+			restartEvents();
+			titles = getTitlesForSponsor();
+			for (String title : titles) {
+				titleCombo.addItem(title);
+			}
+			pack();
+		} catch (SQLException e) {
+		}
+	}
+	
+	
+	private void handleStop() {
+		
+		if (audioPlayer != null)
+			audioPlayer.stopPlaying();
+	}
+	
+	
+	private void handleTitle() {
+		
+		JComboBox yearCombo;
+		Vector<Integer> years;
+		
+		try {
+			pauseEvents();
+			yearCombo = (JComboBox)getInput("Year");
+			yearCombo.removeAllItems();
+			restartEvents();
+			years = getYearsForSponsorTitle();
+			for (Integer year : years) {
+				yearCombo.addItem(year);
+			}
+			pack();
+		} catch (SQLException e) {
+		}
+	}
+	
+	
+	private void handleYear() {
+		
+		try {
+			setDetails();
+		} catch (SQLException e) {
+		}
+	}
+	
+	
+	public Spot getSpot()
+	                    throws SQLException {
+		
+		String sponsor, title;
+		Integer year;
+		
+		sponsor = (String)getItemFrom("Sponsor");
+		title = (String)getItemFrom("Title");
+		year = (Integer)getItemFrom("Year");
+		return retriever.getSpotForSponsorTitleYear(sponsor, title, year);
+	}
+	
+	
+	private void init()
+	                  throws SQLException {
+		
+		// Miscellaneous
+		if (retriever == null)
+			retriever = new Retriever();
+		
+		// Panels
+		initInputs();
+		initButtons();
+	}
+	
+	
+	private void initButtons() {
+		
+		String[] names={"Select","Play","Stop","Refresh","Cancel"};
+		
+		// Add buttons
+		for (String name : names) {
+			addButton(name);
+		}
+	}
+	
+	
+	private void initInputs() 
+	                        throws SQLException {
+		
+		Color color=new Color(0.9f, 0.9f, 0.9f);
+		JTextArea textArea;
+		JTextField textField;
+		
+		// Add editable inputs
+		addInput("Sponsor", new JComboBox(getSponsorsWithSpots()));
+		addInput("Title", new JComboBox(getTitlesForSponsor()));
+		addInput("Year", new JComboBox(getYearsForSponsorTitle()));
+		
+		// Add non-editable inputs
+		textField = new JTextField(30);
+		textField.setEditable(false);
+		textField.setBackground(color);
+		addInput("Filename", textField);
+		textArea = new JTextArea(4, 30);
+		textArea.setEditable(false);
+		textArea.setBackground(color);
+		addInput("Description", textArea);
+		
+		// Initialize
+		setDetails();
+	}
+	
+	
+	private void pauseEvents() {
+		
+		pausedEvents = true;
+	}
+	
+	
+	private void restartEvents() {
+		
+		pausedEvents = false;
+	}
+	
+	
+	private void setDetails()
+	                        throws SQLException {
+		
+		Spot spot;
+		
+		// Set filename
+		spot = getSpot();
+		((JTextField)getInput("Filename")).setText(spot.getFilename());
+		((JTextArea)getInput("Description")).setText(spot.getDescription());
+	}
+	
+	
+	/**
+	 * Test for %SpotSelector.
+	 */
+	public static void main(String[] args) {
+		
+		JFrame frame;
+		SpotSelector dialog;
+		
+		// Start
+		System.out.println();
+		System.out.println("****************************************");
+		System.out.println("SpotSelector");
+		System.out.println("****************************************");
+		System.out.println();
+		
+		try {
+			
+			// Create frame
+			frame = new JFrame("Frame");
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setPreferredSize(new Dimension(640, 480));
+			frame.pack();
+			frame.setVisible(true);
+			
+			// Create dialog
+			dialog = new SpotSelector(frame, "Select Spot");
+			dialog.pack();
+			dialog.setLocationRelativeTo(frame);
+			dialog.setVisible(true);
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		// Finish
+		System.out.println();
+		System.out.println("****************************************");
+		System.out.println("SpotSelector");
+		System.out.println("****************************************");
+		System.out.println();
+	}
+}
+

74d4dc9461b60dbafe3e0b97bc55e506771badea
Andy Brown <adb1413@rit.edu>
2011-03-24 00:45:46 -0400

    Cleaned up SponsorInsertDialog

--- src/SponsorInsertDialog.java
+++ src/SponsorInsertDialog.java
@@ -7,36 +7,27 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.sql.SQLException;
-import java.util.TreeMap;
-import java.util.Vector;
 import javax.swing.*;
 
 
-
-
 /**
- * Dialog window for inserting a %Sponsor.
+ * Dialog window for inserting a Sponsor.
  */
 public class SponsorInsertDialog extends SponsorDialog {
 	
-	
-	public SponsorInsertDialog(Frame frame,
-	                           String title) {
+	public SponsorInsertDialog(Frame frame, String title) {
 		
 		super(frame, title);
 		
-		// Buttons
 		addButton("Insert");
 		addButton("Cancel");
 		addButton("Clear");
 	}
 	
-	
 	public void actionPerformed(ActionEvent event) {
 		
-		String command;
+		String command = event.getActionCommand();
 		
-		command = event.getActionCommand();
 		if (command.equals("Cancel")) {
 			handleCancel();
 		} else if (command.equals("Clear")) {
@@ -46,57 +37,46 @@ public class SponsorInsertDialog extends SponsorDialog {
 		}
 	}
 	
-	
+	/**
+	 * Tries to get sponsor and insert it into the database.
+	 * 
+	 * <p>If doesn't work, user should
+	 * <ul>
+	 *   <li>check if any fields are empty, and
+	 *   <li>check if sponsor is already in system.
+	 * </ul>
+	 */
 	private void handleInsert() {
-		
 		try {
-			
-			// Get sponsor and insert it into database
 			getSponsor().insert();
 			fireActionEvent("Refresh");
 			GUI.showMessage(this, "Inserted sponsor.");
 			setVisible(false);
-		}
-		catch (SQLException e) {
-			String message = "Could not insert sponsor:\n" +
-			                 " - Check if any fields are empty.\n" +
-			                 " - Check if sponsor is already in system.\n";
-			GUI.showError(this, message);
+		} catch (SQLException e) {
+			GUI.showError(this, "Could not insert sponsor!");
 		}
 	}
 	
+	//------------------------------------------------------------
+   // Main
+   //
 	
 	public static void main(String[] args) {
 		
-		JFrame frame;
-		SponsorInsertDialog dialog;
-		
-		// Start
-		System.out.println();
-		System.out.println("****************************************");
-		System.out.println("SponsorInsertDialog");
-		System.out.println("****************************************");
-		System.out.println();
+		JFrame frame = new JFrame("Frame");
+		String title = "Insert Sponsor";
+		SponsorInsertDialog sid = new SponsorInsertDialog(frame, title);
 		
 		// Create window
-		frame = new JFrame("Frame");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setPreferredSize(new Dimension(320, 240));
 		frame.pack();
 		frame.setVisible(true);
 		
 		// Create dialog
-		dialog = new SponsorInsertDialog(frame, "Insert Sponsor");
-		dialog.pack();
-		dialog.setLocationRelativeTo(frame);
-		dialog.setVisible(true);
-		
-		// Finish
-		System.out.println();
-		System.out.println("****************************************");
-		System.out.println("SponsorInsertDialog");
-		System.out.println("****************************************");
-		System.out.println();
+		sid.pack();
+		sid.setLocationRelativeTo(frame);
+		sid.setVisible(true);
 	}
 }
 

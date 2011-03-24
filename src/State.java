9dcf5d3636936d8cc31badaf0a67a2913f88329f
Andy Brown <adb1413@rit.edu>
2011-03-24 01:23:53 -0400

    Cleaned up State

--- src/State.java
+++ src/State.java
@@ -8,7 +8,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
-
 /**
  * Utility for retrieving states from database.
  */
@@ -16,66 +15,51 @@ public class State {
 	
 	private static String[] codes;
 	
-	
 	static {
-		
 		try {
-			initCodes();
+			codes = makeCodes();
 		} catch (SQLException e) {
 			throw new ExceptionInInitializerError("[State] Check database.");
 		}
 	}
 	
-	
-	private static void initCodes()
-	                              throws SQLException {
+	private static String[] makeCodes() throws SQLException {
 		
-		int count, index=-1;
-		ResultSet results;
+		int count;
+		int index = -1;
 		String sql;
+		ResultSet rs;
+		String[] codes;
 		
 		// Count states in database
 		sql = "SELECT COUNT(code) FROM state";
-		results = Database.executeQuery(sql);
-		results.next();
-		count = results.getInt(1);
+		rs = Database.executeQuery(sql);
+		rs.next();
+		count = rs.getInt(1);
 		
 		// Execute query and process results
 		sql = "SELECT code FROM state";
-		results = Database.executeQuery(sql);
+		rs = Database.executeQuery(sql);
 		codes = new String[count];
-		while (results.next()) {
-			codes[++index] = results.getString(1);
+		while (rs.next()) {
+			codes[++index] = rs.getString(1);
 		}
-	}
-	
-	
-	public static String[] getCodes() {
 		
 		return codes;
 	}
 	
+	public static String[] getCodes() {
+		return codes;
+	}
+	
+	//------------------------------------------------------------
+   // Main
+   //
 	
 	public static void main(String[] args) {
-		
-		// Start
-		System.out.println();
-		System.out.println("****************************************");
-		System.out.println("State");
-		System.out.println("****************************************");
-		System.out.println();
-		
-		// Test
 		for (String code : getCodes()) {
 			System.out.println(code);
 		}
-		
-		// Finish
-		System.out.println();
-		System.out.println("****************************************");
-		System.out.println("State");
-		System.out.println("****************************************");
-		System.out.println();
 	}
 }
 

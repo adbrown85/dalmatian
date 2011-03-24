63c7de1e180dad75e8d1d3158a2304fac6117d60
Andy Brown <adb1413@rit.edu>
2011-03-24 00:24:12 -0400

    SlotManager clean up

--- src/SlotManager.java
+++ src/SlotManager.java
@@ -10,7 +10,6 @@ import java.sql.SQLException;
 import javax.swing.*;
 
 
-
 /**
  * Manages slots in a break.
  */
@@ -19,12 +18,9 @@ public class SlotManager extends DatabaseTableManager {
 	Component spotSelectorParent;
 	SpotSelector spotSelector;
 	
-	
-	public SlotManager(Frame frame,
-	                   Break _break)
-	                   throws SQLException {
+	public SlotManager(Frame frame, Break b) throws SQLException {
 		
-		super(new SlotTable(_break));
+		super(new SlotTable(b));
 		
 		// Dialogs
 		spotSelector = new SpotSelector(frame, "Select Spot");
@@ -36,12 +32,10 @@ public class SlotManager extends DatabaseTableManager {
 		addButton("Remove");
 	}
 	
-	
 	public void actionPerformed(ActionEvent event) {
 		
-		String command;
+		String command = event.getActionCommand();
 		
-		command = event.getActionCommand();
 		if (command.equals("Add")) {
 			handleAdd();
 		} else if (command.equals("Remove")) {
@@ -51,31 +45,29 @@ public class SlotManager extends DatabaseTableManager {
 		}
 	}
 	
-	
-	public void commit()
-	                   throws SQLException {
+	public void commit() throws SQLException {
 		
-		Break _break;
+		Break b;
 		String sql;
 		
 		try {
 			
-			// Start transation
+			// Start transaction
 			Database.executeUpdate("START TRANSACTION");
 			
 			// Insert break, or update break and clear slots
-			_break = ((SlotTable)table).getBreak();
-			if (_break.getId() == 0) {
-				_break.insert();
+			b = ((SlotTable)table).getBreak();
+			if (b.getId() == 0) {
+				b.insert();
 			} else {
-				Break.update(_break, _break);
-				sql = "  DELETE FROM slot WHERE break=" + _break.getId();
+				Break.update(b, b);
+				sql = "  DELETE FROM slot WHERE break=" + b.getId();
 				Database.executeUpdate(sql);
 			}
 			
 			// Insert slots back into the break and commit
 			sql = "  INSERT INTO slot(break,position,spot) " + 
-			      "  VALUES " + getValues(_break);
+			      "  VALUES " + getValues(b);
 			Database.executeUpdate(sql);
 			
 			// Commit
@@ -87,42 +79,36 @@ public class SlotManager extends DatabaseTableManager {
 		}
 	}
 	
-	
-	private String getValues(Break _break) {
+	private String getValues(Break b) {
 		
-		int breakId, rowCount;
-		String values="";
+		int id = b.getId();
+		int rowCount = table.getRowCount();
+		StringBuilder sb = new StringBuilder();
 		
-		breakId = _break.getId();
-		rowCount = table.getRowCount();
 		for (int i=0; i<rowCount; ++i) {
-			values += String.format("(%d, %d, %d)",
-			                        breakId,
-			                        i+1,
-			                        table.getValueAt(i,"spot"));
+			sb.append(String.format("(%d, %d, %d)",
+			                        id,
+			                        i + 1,
+			                        table.getValueAt(i, "spot")));
 			if (i < rowCount-1) {
-				values += ", ";
+				sb.append(", ");
 			}
 		}
-		return values;
+		return sb.toString();
 	}
 	
-	
 	private void handleAdd() {
-		
 		spotSelector.pack();
 		spotSelector.setLocationRelativeTo(spotSelectorParent);
 		spotSelector.setVisible(true);
 	}
 	
-	
 	private void handleRemove() {
 		
-		int row;
+		int row = getSelectedRow();
 		
-		row = getSelectedRow();
 		if (row != -1) {
-			((SlotTable)table).remove(getSelectedRow());
+			((SlotTable) table).remove(getSelectedRow());
 		} else {
 			GUI.showMessage(spotSelectorParent, "Please select a slot.");
 		}
@@ -134,83 +120,58 @@ public class SlotManager extends DatabaseTableManager {
 		Spot spot;
 		
 		try {
-			spot = spotSelector.getSpot();
-			((SlotTable)table).add(spot);
+		   spot = spotSelector.getSpot();
+			((SlotTable) table).add(spot);
 		} catch (SQLException e) {
 			GUI.showError(spotSelectorParent, "Could not add spot.");
 		}
 	}
 	
-	
 	public void setBreak(Break _break) {
-		
-		((SlotTable)table).setBreak(_break);
+		((SlotTable) table).setBreak(_break);
 	}
 	
-	
 	public void setSpotSelectorParent(Component component) {
-		
 		this.spotSelectorParent = component;
 	}
 	
+	//------------------------------------------------------------
+   // Main
+   //
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		
-		Box box;
-		JButton button;
-		final JFrame frame;
-		final SlotManager slotManager;
+		Box box = new Box(BoxLayout.PAGE_AXIS);
+		JButton button = new JButton("Commit");
+		final JFrame frame = new JFrame("Slot Manager");
+		final SlotManager sm = new SlotManager(frame, new Break(12));
 		
-		// Start
-		System.out.println();
-		System.out.println("****************************************");
-		System.out.println("SlotManager");
-		System.out.println("****************************************");
-		System.out.println();
+		// Make frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		try {
-			
-			// Make frame
-			frame = new JFrame("Slot Manager");
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			
-			// Make slot manager in box
-			box = new Box(BoxLayout.PAGE_AXIS);
-			slotManager = new SlotManager(frame, new Break(12));
-			slotManager.setBorder(BorderFactory.createTitledBorder("Slots"));
-			box.add(slotManager);
-			
-			// Add commit button
-			button = new JButton("Commit");
-			button.setAlignmentX(0.5f);
-			box.add(button);
-			button.addActionListener(new ActionListener(){
-				public void actionPerformed(ActionEvent event) {
-					try {
-						slotManager.commit();
-						GUI.showMessage(frame, "Committed.");
-					} catch (SQLException e) {
-						GUI.showError(frame, "Could not commit.");
-						e.printStackTrace();
-					}
+		// Make slot manager in box
+		sm.setBorder(BorderFactory.createTitledBorder("Slots"));
+		box.add(sm);
+		
+		// Add commit button
+		button.setAlignmentX(0.5f);
+		box.add(button);
+		button.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent event) {
+				try {
+					sm.commit();
+					GUI.showMessage(frame, "Committed.");
+				} catch (SQLException e) {
+					GUI.showError(frame, "Could not commit.");
+					e.printStackTrace();
 				}
-			});
-			
-			// Show in frame
-			frame.setContentPane(box);
-			frame.pack();
-			frame.setVisible(true);
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
+			}
+		});
 		
-		// Finish
-		System.out.println();
-		System.out.println("****************************************");
-		System.out.println("SlotManager");
-		System.out.println("****************************************");
-		System.out.println();
+		// Show in frame
+		frame.setContentPane(box);
+		frame.pack();
+		frame.setVisible(true);
 	}
 }
 

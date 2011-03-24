59e25aa1c1a3f1bf4f3d954b54b01bb175cb2527
Andy Brown <adb1413@rit.edu>
2011-03-23 23:24:25 -0400

    Cleaned up HidableDatabaseTableModel

--- src/HidableDatabaseTableModel.java
+++ src/HidableDatabaseTableModel.java
@@ -4,35 +4,27 @@
  * Author
  *     Andrew Brown <andrew@andrewdbrown.com>
  */
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Types;
-import java.util.Arrays;
-import java.util.TreeMap;
 import java.util.Vector;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.table.AbstractTableModel;
-
 
 
 /**
  * TableModel for viewing data from a database where columns can be hidden.
  * 
- * Takes an array of column names that should be hidden and makes a mapping of 
- * visible columns to the actual columns in the data.  The overrided methods 
- * of getValueAt(), getColumnCount(), and getColumnName() then use this 
- * mapping.  Values from hidden columns can be obtained by using the actual 
- * column name.
+ * <p>Takes an array of column names that should be hidden and makes a
+ * mapping of visible columns to the actual columns in the data.  The
+ * overridden methods of getValueAt(), getColumnCount(), and
+ * getColumnName() then use this mapping.  Values from hidden columns
+ * can be obtained by using the actual column name.
  */
 public class HidableDatabaseTableModel extends DatabaseTableModel {
 	
 	protected int columnCountVisible;
 	private Vector<Integer> hiddenColumnIndices;
 	private int[] visibleToDataColumns;
-	
 	
 	public HidableDatabaseTableModel(String sql,
 	                                 String[] hiddenColumnNames)
@@ -46,97 +38,92 @@ public class HidableDatabaseTableModel extends DatabaseTableModel {
 		initVisibleToDataColumns();
 	}
 	
+   //------------------------------------------------------------
+   // Getters
+   //
+   
+   public int getDataColumn(int columnIndex) {
+      return visibleToDataColumns[columnIndex];
+   }
+   
+   public Object getValueAt(int rowIndex,
+                            int columnIndex) {
+      return data.get(rowIndex)[getDataColumn(columnIndex)];
+   }
+   
+   public int getColumnCount() {
+      return columnCountVisible;
+   }
+   
+   public String getColumnName(int columnIndex) {
+      return columnNames[getDataColumn(columnIndex)];
+   }
+   
+	//------------------------------------------------------------
+   // Helpers
+   //
 	
-	protected int getDataColumn(int columnIndex) {
-		
-		return visibleToDataColumns[columnIndex];
-	}
-	
-	
-	public Object getValueAt(int rowIndex,
-	                         int columnIndex) {
-		
-		return data.get(rowIndex)[getDataColumn(columnIndex)];
-	}
-	
-	
-	public int getColumnCount() {
-		
-		return columnCountVisible;
-	}
-	
-	
-	public String getColumnName(int columnIndex) {
-		
-		return columnNames[getDataColumn(columnIndex)];
-	}
-	
-	
-	private boolean hasHiddenColumns() {
-		
-		return hiddenColumnIndices != null && !hiddenColumnIndices.isEmpty();
-	}
-	
-	
-	private void initColumnCountVisible() {
-		
-		if (hasHiddenColumns()) {
-			columnCountVisible = columnCount - hiddenColumnIndices.size();
-		} else {
-			columnCountVisible = columnCount;
-		}
-	}
-	
-	
-	private void initHiddenColumnIndices(String[] hiddenColumnNames) {
-		
-		// Test if empty
-		if (hiddenColumnNames == null) {
-			hiddenColumnIndices = null;
-			return;
-		}
-		
-		// Store indices of hidden columns
-		hiddenColumnIndices = new Vector<Integer>();
-		for (String columnName : hiddenColumnNames) {
-			Integer columnIndex = getColumnIndex(columnName);
-			if (columnIndex != null)
-				hiddenColumnIndices.add(columnIndex);
-		}
-	}
-	
-	
-	private void initVisibleToDataColumns() {
-		
-		// Allocate
-		visibleToDataColumns = new int[columnCountVisible];
-		
-		// If hidden columns, map visible columns to data columns
-		if (hasHiddenColumns()) {
-			int visibleColumn = -1;
-			for (int i=0; i<columnCount; ++i) {
-				if (isColumnVisible(i)) {
-					visibleToDataColumns[++visibleColumn] = i;
-				}
-			}
-		}
-		
-		// Otherwise mapping is straight
-		else {
-			for (int i=0; i<columnCount; ++i) {
-				visibleToDataColumns[i] = i;
-			}
-		}
-	}
-	
-	
-	private boolean isColumnVisible(int columnIndex) {
-		
-		if (hiddenColumnIndices == null)
-			return true;
-		return !hiddenColumnIndices.contains(columnIndex);
-	}
-	
+   private boolean hasHiddenColumns() {
+      return hiddenColumnIndices != null && !hiddenColumnIndices.isEmpty();
+   }
+   
+   private void initColumnCountVisible() {
+      if (hasHiddenColumns()) {
+         columnCountVisible = columnCount - hiddenColumnIndices.size();
+      } else {
+         columnCountVisible = columnCount;
+      }
+   }
+   
+   private void initHiddenColumnIndices(String[] hiddenColumnNames) {
+      
+      // Test if empty
+      if (hiddenColumnNames == null) {
+         hiddenColumnIndices = null;
+         return;
+      }
+      
+      // Store indices of hidden columns
+      hiddenColumnIndices = new Vector<Integer>();
+      for (String columnName : hiddenColumnNames) {
+         Integer columnIndex = getColumnIndex(columnName);
+         if (columnIndex != null)
+            hiddenColumnIndices.add(columnIndex);
+      }
+   }
+   
+   private void initVisibleToDataColumns() {
+      
+      // Allocate
+      visibleToDataColumns = new int[columnCountVisible];
+      
+      // If hidden columns, map visible columns to data columns
+      if (hasHiddenColumns()) {
+         int visibleColumn = -1;
+         for (int i=0; i<columnCount; ++i) {
+            if (isColumnVisible(i)) {
+               visibleToDataColumns[++visibleColumn] = i;
+            }
+         }
+      }
+      
+      // Otherwise mapping is straight
+      else {
+         for (int i=0; i<columnCount; ++i) {
+            visibleToDataColumns[i] = i;
+         }
+      }
+   }
+   
+   private boolean isColumnVisible(int columnIndex) {
+      if (hiddenColumnIndices == null)
+         return true;
+      return !hiddenColumnIndices.contains(columnIndex);
+   }
+   
+	//------------------------------------------------------------
+   // Main
+   //
 	
 	public static void main(String[] args) {
 		

2eb5347ec94060979b5f82629b9b2ac76aaec8f1
Andy Brown <adb1413@rit.edu>
2011-03-23 21:32:31 -0400

    Cleaned up Joystick a bit

--- src/Joystick.java
+++ src/Joystick.java
@@ -9,23 +9,18 @@ import java.util.Vector;
 import net.java.games.input.*;
 
 
-
 public class Joystick {
 	
 	Component buttons[];
 	Controller controller;
 	Vector<JoystickListener> listeners;
 	
-	
-	
 	/**
 	 * Creates a reference to a joystick attached to the system.
 	 * 
-	 * @throws IOException
-	 *     If no joystick can be found.
+	 * @throws IOException if no joystick can be found
 	 */
-	public Joystick()
-	                throws IOException {
+	public Joystick() throws IOException {
 		
 		// Find first joystick
 		controller = findJoystick(controllers);
@@ -37,80 +32,65 @@ public class Joystick {
 		listeners = new Vector<JoystickListener>();
 	}
 	
-	
-	
 	public void addListener(JoystickListener l) {
-		
-		// Add listener
 		listeners.add(l);
 	}
 	
-	
-	
 	private void initButtons() {
 		
-		Component components[];
-		int count=0, index=0;
-		
-		// Get components of joystick
-		components = controller.getComponents();
+		Component components[] = controller.getComponents();
+		int count = 0;
+		int index = 0;
 		
 		// Count how many are buttons
-		for (Component c : components)
-			if (isButton(c))
+		for (Component c : components) {
+			if (isButton(c)) {
 				count++;
+			}
+		}
 		
 		// Build array
 		buttons = new Component[count];
-		for (Component c : components)
-			if (isButton(c))
+		for (Component c : components) {
+			if (isButton(c)) {
 				buttons[index++] = c;
+			}
+		}
 	}
-	
-	
 	
 	/**
 	 * Initializes the reference to the controller.
 	 * 
-	 * @throws IOException
-	 *     If no joystick controller can be found.
+	 * @throws IOException if no joystick controller can be found
 	 */
-	private void initController()
-	                            throws IOException {
+	private void initController() throws IOException {
 		
-		ControllerEnvironment environment;
-		Controller controllers[];
-		
-		// Get controllers
-		environment = ControllerEnvironment.getDefaultEnvironment();
-		controllers = environment.getControllers();
+		ControllerEnvironment env = getDefaultControllerEnvironment();
+		Controller controllers[] = env.getControllers();
 		
 		// Find first joystick in controllers
 		controller = null;
-		for (Controller c : controllers)
-			if (c.getType() == Controller.Type.STICK)
+		for (Controller c : controllers) {
+			if (c.getType() == Controller.Type.STICK) {
 				controller = c;
-		if (controller == null)
+			}
+		}
+		
+		// Make sure it was found
+		if (controller == null) {
 			throw new IOException("No joystick found.");
+		}
 	}
 	
-	
-	
 	private static boolean isButton(Component c) {
-		
-		// Check if component is a button
 		return c.getIdentifier() instanceof Component.Identifier.Button;
 	}
 	
+	private static ControllerEnvironment getDefaultControllerEnvironment() {
+	   return ControllerEnvironment.getDefaultEnvironment();
+	}
 	
-	
-	/**
-	 * 
-	 */
-	public void poll(int delay)
-	                 throws Exception {
-		
-		// Poll buttons
+	public void poll(int delay) throws Exception {
 		while (controller.poll()) {
 			for (Component b : buttons) {
 				if (b.getPollData() == 1.0) {

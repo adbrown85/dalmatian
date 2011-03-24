ee7c80e6461151f09321002c8f9bb93906c2aa0c
Andy Brown <adb1413@rit.edu>
2011-03-23 21:48:42 -0400

    Cleaned up ActionDialog a bit

--- src/ActionDialog.java
+++ src/ActionDialog.java
@@ -10,91 +10,76 @@ import java.util.Vector;
 import javax.swing.*;
 
 
-
 /**
  * Dialog that's good at dealing with action events.
  * 
- * ActionDialog supports listeners with addActionListener() and 
- * fireActionEvent(). Any listeners registered with addActionListener() will 
- * receive an ActionEvent if the class calls fireActionEvent().
+ * <p>ActionDialog supports listeners with addActionListener() and
+ * fireActionEvent(). Any listeners registered with addActionListener()
+ * will receive an ActionEvent if the class calls fireActionEvent().
  * 
- * Furthermore ActionDialog provides a simple actionPerformed() method for 
- * receiving ActionEvents from other classes.  It simply prints the 
- * ActionEvent's action command.
+ * <p>Furthermore ActionDialog provides a simple actionPerformed()
+ * method for receiving ActionEvents from other classes.  It simply
+ * prints the ActionEvent's action command.
  * 
- * Also, sometimes it's necessary to pause events when the dialog is changing 
- * itself around to avoid sending unnecessary ActionEvents.  ActionDialog adds 
- * pauseEvents(), restartEvents(), and eventsArePaused() for that reason.
+ * <p>Also, sometimes it's necessary to pause events when the dialog is
+ * changing itself around to avoid sending unnecessary ActionEvents.
+ * ActionDialog adds pauseEvents(), restartEvents(), and 
+ * eventsArePaused() for that reason.
  */
 public class ActionDialog extends JDialog
                           implements ActionListener {
 	
-	private boolean pausedEvents=false;
 	protected final Frame frame;
 	protected final Vector<ActionListener> listeners;
+	private boolean pausedEvents;
 	
-	
-	public ActionDialog(Frame frame,
-	                    String title) {
+	public ActionDialog(Frame frame, String title) {
 		
 		super(frame, title, true);
 		
-		// Initialize
+		this.pausedEvents = false;
 		this.frame = frame;
-		listeners = new Vector<ActionListener>();
+		this.listeners = new Vector<ActionListener>();
 	}
 	
-	
 	public void actionPerformed(ActionEvent event) {
-		
 		if (!eventsArePaused()) {
-			System.err.printf("[ActionDialog] Received \"%s\".\n",
-			                  event.getActionCommand());
+			System.err.printf(
+			      "[ActionDialog] Received \"%s\".\n",
+			      event.getActionCommand());
 		}
 	}
 	
-	
 	public final void addActionListener(ActionListener listener) {
-		
 		listeners.add(listener);
 	}
 	
-	
 	protected boolean eventsArePaused() {
-		
 		return pausedEvents;
 	}
 	
-	
 	protected final void fireActionEvent(String command) {
 		
-		ActionEvent event;
+		ActionEvent event = new ActionEvent(this, 0, command);
 		
-		// Send to each listener
-		event = new ActionEvent(this, 0, command);
 		for (ActionListener listener : listeners) {
 			listener.actionPerformed(event);
 		}
 	}
 	
-	
 	protected void pauseEvents() {
-		
 		pausedEvents = true;
 	}
 	
-	
 	protected void restartEvents() {
-		
 		pausedEvents = false;
 	}
 	
-	
 	public static void main(String[] args) {
 		
-		JButton button;
-		JFrame frame;
-		ActionDialog dialog;
+		JButton button = new JButton("Action");
+		JFrame frame = new JFrame("Frame");
+		ActionDialog dialog = new ActionDialog(frame, "Button Dialog");
 		
 		// Start
 		System.out.println();
@@ -104,15 +89,12 @@ public class ActionDialog extends JDialog
 		System.out.println();
 		
 		// Create frame
-		frame = new JFrame("Frame");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setPreferredSize(new Dimension(400, 300));
 		frame.pack();
 		frame.setVisible(true);
 		
 		// Create dialog with button
-		dialog = new ActionDialog(frame, "Button Dialog");
-		button = new JButton("Action");
 		button.setActionCommand("Action");
 		dialog.add(button);
 		button.addActionListener(dialog);

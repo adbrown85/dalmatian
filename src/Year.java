7f87fce9d32f595874a3ef5783bd39e0844b5f0b
Andy Brown <adb1413@rit.edu>
2011-03-24 01:31:08 -0400

    Cleaned up Year

--- src/Year.java
+++ src/Year.java
@@ -6,57 +6,36 @@
  */
 
 
-
-/**
- * 
- */
 public class Year {
 	
 	private static final Integer[] years;
 	
-	
 	static {
 		
-		int first=2010, last=2025;
-		int index=-1;
+		int first = 2010;
+		int last = 2025;
+		int len = last - first + 1;
+		int index = -1;
 		
 		// Add years
-		years = new Integer[last-first+1];
+		years = new Integer[len];
 		for (int y=first; y<=last; ++y) {
 			years[++index] = y;
 		}
 	}
 	
-	
 	public static Integer[] getAllYears() {
-		
 		return years;
 	}
 	
+	//------------------------------------------------------------
+   // Main
+   //
 	
-	/**
-	 * Test for Year.
-	 */
 	public static void main(String[] args) {
-		
-		// Start
-		System.out.println();
-		System.out.println("****************************************");
-		System.out.println("Year");
-		System.out.println("****************************************");
-		System.out.println();
-		
-		// Test
 		for (int year : Year.getAllYears()) {
 			System.out.println(year);
 		}
-		
-		// Finish
-		System.out.println();
-		System.out.println("****************************************");
-		System.out.println("Year");
-		System.out.println("****************************************");
-		System.out.println();
 	}
 }
 

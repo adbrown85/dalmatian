b27c380748a2551af805689a8d12fb48b2b1dc5c
Andy Brown <adb1413@rit.edu>
2011-03-23 23:20:56 -0400

    Cleaned up GUI

--- src/GUI.java
+++ src/GUI.java
@@ -5,145 +5,137 @@
  *     Andrew Brown <andrew@andrewdbrown.com>
  */
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.*;
 
 
-
 /**
- * GUI utilties.
+ * GUI utilities.
  */
 public class GUI {
 	
-	
 	public static Dimension getInfiniteSize() {
-		
 		return new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE);
 	}
 	
-	
-	public static JComboBox getComboBoxFor(int start,
-	                                       int end) {
+	public static JComboBox getComboBoxFor(int start, int end) {
 		
-		JComboBox comboBox;
+		JComboBox cb = new JComboBox();
 		
-		// Make range
-		comboBox = new JComboBox();
-		for (int i=start; i<=end; ++i)
-			comboBox.addItem(i);
-		return comboBox;
+		for (int i=start; i<=end; ++i) {
+			cb.addItem(i);
+		}
+		return cb;
 	}
-	
 	
 	public static JScrollPane getScrollPaneFor(Component component) {
-		
 		return getScrollPaneFor(component, 0);
 	}
-	
 	
 	public static JScrollPane getScrollPaneFor(Component component,
 	                                           int borderWidth) {
 		
-		int policy=ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS;
-		JScrollPane scrollPane;
+		int policy = ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS;
+		JScrollPane sp = new JScrollPane(component);
 		
-		scrollPane = new JScrollPane(component);
-		scrollPane.setMaximumSize(getInfiniteSize());
-		scrollPane.setVerticalScrollBarPolicy(policy);
-		if (borderWidth > 0)
-			setScrollPaneBorder(scrollPane, borderWidth);
-		return scrollPane;
+		sp.setMaximumSize(getInfiniteSize());
+		sp.setVerticalScrollBarPolicy(policy);
+		if (borderWidth > 0) {
+			setScrollPaneBorder(sp, borderWidth);
+		}
+		return sp;
 	}
-	
 	
 	public static void setScrollPaneBorder(JScrollPane scrollPane,
 	                                       int width) {
 		
-		Border border;
+		Border border = createEmptyBorder(width);
 		
-		border = BorderFactory.createEmptyBorder(width, width, width, width);
 		setScrollPaneBorder(scrollPane, border);
 	}
-	
 	
 	public static void setScrollPaneBorder(JScrollPane scrollPane,
 	                                       Border border) {
 		
-		Border compound, inside;
+		Border inside = scrollPane.getBorder();
+		Border compound = createCompoundBorder(border, inside);
 		
-		inside = scrollPane.getBorder();
-		compound = BorderFactory.createCompoundBorder(border, inside);
 		scrollPane.setBorder(compound);
 	}
 	
-	
 	public static boolean showConfirmDelete(Component parent) {
 		
-		int option, style;
-		String message, title;
+		int style = JOptionPane.YES_NO_OPTION;
+		String message = "Are you sure you want to delete this item?";
+		String title = "Confirm Delete";
+		int option = showConfirmDialog(parent, message, title, style);
 		
-		// Show confirm dialog
-		title = "Confirm Delete";
-		message = "Are you sure you want to delete this item?";
-		style = JOptionPane.YES_NO_OPTION;
-		option = JOptionPane.showConfirmDialog(parent, message, title, style);
 		return option == JOptionPane.YES_OPTION;
 	}
 	
-	
-	/**
-	 * Shows an error dialog.
-	 */
-	public static void showError(Component parent,
-	                             String message) {
+   private static int showConfirmDialog(Component parent,
+                                        String message,
+                                        String title,
+                                        int style) {
+      return JOptionPane.showConfirmDialog(parent, message, title, style);
+   }
+   
+	public static void showError(Component parent, String message) {
 		
-		int type;
-		String title;
+		int type = JOptionPane.ERROR_MESSAGE;
+		String title = "Error";
 		
-		type = JOptionPane.ERROR_MESSAGE;
-		title = "Error";
 		JOptionPane.showMessageDialog(parent, message, title, type);
 	}
 	
-	
-	/**
-	 * Shows a message dialog.
-	 */
 	public static void showMessage(Component parent,
 	                               String message) {
-		
 		JOptionPane.showMessageDialog(parent, message);
 	}
 	
+	//------------------------------------------------------------
+   // Helpers
+   //
 	
-	public static void main(String[] args) {
-		
-		JFrame frame;
-		
-		// Start
-		System.out.println();
-		System.out.println("****************************************");
-		System.out.println("GUI");
-		System.out.println("****************************************");
-		System.out.println();
-		
-		// Create frame
-		frame = new JFrame("Frame");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setPreferredSize(new Dimension(320, 240));
-		frame.pack();
-		frame.setVisible(true);
-		
-		// Show error
-		GUI.showError(frame, "This is an error!");
-		
-		// Finish
-		System.out.println();
-		System.out.println("****************************************");
-		System.out.println("GUI");
-		System.out.println("****************************************");
-		System.out.println();
-	}
+   private static Border createEmptyBorder(int width) {
+      return BorderFactory.createEmptyBorder(width, width, width, width);
+   }
+   
+   private static Border createCompoundBorder(Border ob, Border ib) {
+      return BorderFactory.createCompoundBorder(ob, ib);
+   }
+   
+   //------------------------------------------------------------
+   // Main
+   //
+   
+   public static void main(String[] args) {
+      
+      JFrame frame;
+      
+      // Start
+      System.out.println();
+      System.out.println("****************************************");
+      System.out.println("GUI");
+      System.out.println("****************************************");
+      System.out.println();
+      
+      // Create frame
+      frame = new JFrame("Frame");
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setPreferredSize(new Dimension(320, 240));
+      frame.pack();
+      frame.setVisible(true);
+      
+      // Show error
+      GUI.showError(frame, "This is an error!");
+      
+      // Finish
+      System.out.println();
+      System.out.println("****************************************");
+      System.out.println("GUI");
+      System.out.println("****************************************");
+      System.out.println();
+   }
 }
 

d910244a99dcb0b3c3e028205fc5fc2bed8be9a0
Andy Brown <adb1413@rit.edu>
2011-03-23 23:42:26 -0400

    Cleaned up InputPanel

--- src/InputPanel.java
+++ src/InputPanel.java
@@ -4,70 +4,65 @@
  * Author
  *     Andrew Brown <andrew@andrewdbrown.com>
  */
-import java.awt.*;
+import java.awt.Component;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.*;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.util.TreeMap;
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import javax.swing.*;
 import javax.swing.text.JTextComponent;
-
 
 
 /**
  * Panel containing input fields with labels.
  */
-public class InputPanel extends JPanel
-                        implements ActionListener {
+public class InputPanel extends JPanel implements ActionListener {
 	
-	protected int row=-1;
-	protected TreeMap<String,JComponent> inputs;
-	protected Vector<ActionListener> listeners;
-	
+   protected final List<ActionListener> listeners;
+	protected final Map<String,JComponent> inputs;
+	protected int row;
 	
 	/**
 	 * Creates a new, empty input panel.
 	 * 
-	 * @param title
-	 *     Text for the titled border around the panel.
+	 * @param title Text for the titled border around the panel
 	 */
 	public InputPanel(String title) {
 		
-		// Initialize
 		super(new GridBagLayout());
+		
+		this.row = -1;
+      this.inputs = new HashMap<String,JComponent>();
+      this.listeners = new ArrayList<ActionListener>();
+		
 		setBorder(BorderFactory.createTitledBorder(title));
-		inputs = new TreeMap<String,JComponent>();
-		listeners = new Vector<ActionListener>();
 	}
 	
-	
 	public void actionPerformed(ActionEvent event) {
-		
 		for (ActionListener listener : listeners) {
 			listener.actionPerformed(event);
 		}
 	}
 	
-	
 	public void addActionListener(ActionListener listener) {
-		
 		listeners.add(listener);
 	}
-	
 	
 	/**
 	 * Adds a label to the input panel.
 	 */
-	protected void addLabel(String name,
-	                        int row,
-	                        int anchor) {
+	protected void addLabel(String name, int row, int anchor) {
 		
-		JLabel label;
-		GridBagConstraints gbc;
+		JLabel label = new JLabel(name);
+		GridBagConstraints gbc = new GridBagConstraints();
 		
 		// Setup constraints
-		gbc = new GridBagConstraints();
 		gbc.anchor = anchor;
 		gbc.gridx = 0;
 		gbc.gridy = row;
@@ -75,26 +70,21 @@ public class InputPanel extends JPanel
 		gbc.insets = new Insets(2, 4, 2, 4);
 		
 		// Add component
-		label = new JLabel(name);
 		add(label, gbc);
 	}
-	
 	
 	/**
 	 * Adds an input to the panel with a label.
 	 * 
-	 * If the input is an instance of %JTextArea, it will be wrapped in an 
-	 * instance of %JScrollPane for display.  Note that getInput will still 
-	 * return the %JTextArea.  Also, it will automatically enable word 
-	 * wrapping at word breaks on the text area.
+	 * <p>If the input is an instance of %JTextArea, it will be wrapped
+	 * in an instance of JScrollPane for display.  Note that getInput
+	 * will still return the JTextArea.  Also, it will automatically
+	 * enable word wrapping at word breaks on the text area.
 	 * 
-	 * @param name
-	 *     Text for the label.
-	 * @param input
-	 *     Component that accepts input from the user.
+	 * @param name Text for the label
+	 * @param input Component that accepts input from the user
 	 */
-	public void addInput(String name,
-	                     JComponent input) {
+	public void addInput(String name, JComponent input) {
 		
 		int anchor;
 		
@@ -111,18 +101,14 @@ public class InputPanel extends JPanel
 		addInput(name, row, input);
 	}
 	
-	
 	/**
 	 * Adds an input component to the panel and stores it.
 	 */
-	protected void addInput(String name,
-	                        int row,
-	                        JComponent input) {
+	protected void addInput(String name, int row, JComponent input) {
 		
-		GridBagConstraints gbc;
+		GridBagConstraints gbc = new GridBagConstraints();
 		
 		// Setup constraints
-		gbc = new GridBagConstraints();
 		gbc.anchor = GridBagConstraints.LINE_START;
 		gbc.gridx = 1;
 		gbc.gridy = row;
@@ -135,159 +121,173 @@ public class InputPanel extends JPanel
 		// Add and store component
 		inputs.put(name, input);
 		if (input instanceof JTextComponent) {
-			((JTextComponent)input).setMargin(new Insets(0,2,0,2));
+			((JTextComponent) input).setMargin(new Insets(0,2,0,2));
 		}
 		if (input instanceof JTextArea) {
-			((JTextArea)input).setLineWrap(true);
-			((JTextArea)input).setWrapStyleWord(true);
+			((JTextArea) input).setLineWrap(true);
+			((JTextArea) input).setWrapStyleWord(true);
 			input = new JScrollPane(input);
 		}
 		add(input, gbc);
 		
 		// Add listener
 		if (input instanceof JComboBox) {
-			((JComboBox)input).setActionCommand(name);
-			((JComboBox)input).addActionListener(this);
+			((JComboBox) input).setActionCommand(name);
+			((JComboBox) input).addActionListener(this);
 		}
 	}
-	
 	
 	/**
 	 * Clears all text components and resets combo boxes.
 	 */
 	public void clear() {
-		
 		for (JComponent input : inputs.values()) {
 			if (input instanceof JTextComponent) {
-				((JTextComponent)input).setText(null);
+				((JTextComponent) input).setText(null);
 			} else if (input instanceof JComboBox) {
-				((JComboBox)input).setSelectedIndex(0);
+				((JComboBox) input).setSelectedIndex(0);
 			} else if (input instanceof FilenameInput) {
-				((FilenameInput)input).setText(null);
+				((FilenameInput) input).setText(null);
 			}
 		}
 	}
 	
+	//------------------------------------------------------------
+   // Getters
+   //
 	
 	/**
-	 * Gets one of the inputs according to its name.
+	 * Returns one of the inputs according to its name.
+	 * 
+	 * @param name Name of input field
+	 * @return Input field, or <tt>null</tt> if not stored
 	 */
 	public JComponent getInput(String name) {
-		
 		return inputs.get(name);
 	}
 	
-	
-	public Object getItemFrom(String inputName) {
+	/**
+	 * Returns an item from a combo box.
+	 * 
+	 * @param name Name of the combo box
+	 * @return Selected item, or <tt>null</tt> if N/A
+	 */
+	public Object getItemFrom(String name) {
 		
-		JComponent input;
+		JComponent input = getInput(name);
 		
-		input = getInput(inputName);
 		if (input instanceof JComboBox) {
-			return ((JComboBox)input).getSelectedItem();
+			return ((JComboBox) input).getSelectedItem();
 		} else {
 			return null;
 		}
 	}
 	
-	
-	public String getTextFrom(String inputName) {
+	/**
+	 * Returns the text in an input field.
+	 * 
+	 * @param name Name of the input field
+	 * @return Text in field, or <tt>null</tt> if N/A or text is empty
+	 */
+	public String getTextFrom(String name) {
 		
-		JComponent input;
+		JComponent input = getInput(name);
 		String text;
 		
 		// Get text
-		input = getInput(inputName);
 		if (input instanceof FilenameInput) {
-			text = ((FilenameInput)input).getText();
+			text = ((FilenameInput) input).getText();
 		} else if (input instanceof JTextComponent) {
-			text = ((JTextComponent)input).getText();
+			text = ((JTextComponent) input).getText();
 		} else {
 			return null;
 		}
-		
-		// Return null if it's empty
-		if (text.isEmpty()) {
-			return null;
-		} else {
-			return text;
-		}
+		return (text.isEmpty()) ? null : text;
 	}
 	
-	
-	public Timestamp getTimestampFrom(String inputName) {
+	/**
+	 * Returns the timestamp in an input field.
+	 * 
+	 * @param name name of the input field
+	 * @return Timestamp in field, or <tt>null</tt> if N/A
+	 */
+	public Timestamp getTimestampFrom(String name) {
 		
-		JComponent input;
+		JComponent input = getInput(name);
 		
-		input = getInput(inputName);
 		if (input instanceof TimestampEditor) {
-			return ((TimestampEditor)input).getTimestamp();
+			return ((TimestampEditor) input).getTimestamp();
 		} else {
 			return null;
 		}
 	}
 	
-	
-	public void setTextIn(String inputName,
-	                      String text) {
+	/**
+	 * Changes the text in an input field.
+	 * 
+	 * @param name Name of the input field
+	 * @param text Text to change input field to
+	 */
+	public void setTextIn(String name, String text) {
 		
-		JComponent input;
+		JComponent input = getInput(name);
 		
-		input = getInput(inputName);
 		if (input instanceof FilenameInput) {
-			((FilenameInput)input).setText(text);
+			((FilenameInput) input).setText(text);
 		} else if (input instanceof JTextComponent) {
-			((JTextComponent)input).setText(text);
+			((JTextComponent) input).setText(text);
 		}
 	}
 	
-	
-	public void setItemIn(String inputName,
-	                      Object item) {
+	/**
+	 * Changes the selected item in a combo box.
+	 * 
+	 * @param name Name of the combo box
+	 * @param item Item to select
+	 */
+	public void setItemIn(String name, Object item) {
 		
-		JComponent input;
+		JComponent input = getInput(name);
 		
-		input = getInput(inputName);
 		if (input instanceof JComboBox) {
-			((JComboBox)input).setSelectedItem(item);
+			((JComboBox) input).setSelectedItem(item);
 		}
 	}
 	
-	
-	public void setTimestampIn(String inputName,
+	/**
+	 * Changes the timestamp in an input field.
+	 * 
+	 * @param name Name of the input field
+	 * @param timestamp Timestamp to change field to
+	 * @throws SQLException if timestamp is invalid
+	 */
+	public void setTimestampIn(String name,
 	                           Timestamp timestamp)
 	                           throws SQLException {
 		
-		JComponent input;
+		JComponent input = getInput(name);
 		
-		input = getInput(inputName);
 		if (input instanceof TimestampEditor) {
-			((TimestampEditor)input).setTimestamp(timestamp);
+			((TimestampEditor) input).setTimestamp(timestamp);
 		}
 	}
 	
+	//------------------------------------------------------------
+   // Main
+   //
 	
 	public static void main(String[] args) {
 		
-		final InputPanel panel;
-		JButton button;
-		JPanel pane;
-		JFrame frame;
-		
-		// Start
-		System.out.println();
-		System.out.println("****************************************");
-		System.out.println("InputPanel");
-		System.out.println("****************************************");
-		System.out.println();
+		final InputPanel panel = new InputPanel("Input");
+		JButton button = new JButton("Clear");
+		JPanel pane = new JPanel();
+		JFrame frame = new JFrame("Input Panel");
 		
 		// Create input panel
-		panel = new InputPanel("Input");
 		panel.addInput("Name", new JTextField(20));
 		panel.addInput("Description", new JTextArea(4, 28));
 		
 		// Create button
-		button = new JButton("Clear");
 		button.setAlignmentX(Component.CENTER_ALIGNMENT);
 		button.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent event) {
@@ -296,24 +296,15 @@ public class InputPanel extends JPanel
 		});
 		
 		// Setup pane
-		pane = new JPanel();
 		pane.setLayout(new BoxLayout(pane, BoxLayout.PAGE_AXIS));
 		pane.add(panel);
 		pane.add(button);
 		
 		// Create and show frame
-		frame = new JFrame("Input Panel");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setContentPane(pane);
 		frame.pack();
 		frame.setVisible(true);
-		
-		// Finish
-		System.out.println();
-		System.out.println("****************************************");
-		System.out.println("InputPanel");
-		System.out.println("****************************************");
-		System.out.println();
 	}
 }
 

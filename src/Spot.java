6ac726786a393ff08c1c8de42b199a511ea17d04
Andy Brown <adb1413@rit.edu>
2011-03-24 01:00:08 -0400

    Cleaned up Spot

--- src/Spot.java
+++ src/Spot.java
@@ -4,13 +4,9 @@
  * Author
  *     Andrew Brown <andrew@andrewdbrown.com>
  */
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
 
 
 /**
@@ -18,37 +14,29 @@ import java.sql.SQLException;
  */
 public class Spot {
 	
-	private static PreparedStatement deleteStatement,
-	                                 insertStatement,
-	                                 selectStatement,
-	                                 updateStatement;
+	private static final PreparedStatement deleteStatement;
+	private static final PreparedStatement insertStatement;
+	private static final PreparedStatement selectStatement;
+	private static final PreparedStatement updateStatement;
+	
 	private int id, year;
 	private String title, filename, description, sponsor;
 	
-	
 	/**
-	 * Static constructor.
+	 * Initializes static fields.
 	 */
 	static {
-		
-		String message;
-		
 		try {
-			initDeleteStatement();
-			initInsertStatement();
-			initSelectStatement();
-			initUpdateStatement();
+			deleteStatement = makeDeleteStatement();
+			insertStatement = makeInsertStatement();
+			selectStatement = makeSelectStatement();
+			updateStatement = makeUpdateStatement();
 		} catch (SQLException e) {
-			message = "[Spot] Could not prepare statements.\n" + 
-			          "[Spot] Check database connection.";
-			throw new ExceptionInInitializerError(message);
+			throw new ExceptionInInitializerError("Could not make statements!");
 		}
 	}
 	
-	
 	public Spot() {
-		
-		// Initialize
 		id = 0;
 		description = null;
 		filename = null;
@@ -57,10 +45,7 @@ public class Spot {
 		year = 0;
 	}
 	
-	
 	public Spot(Spot other) {
-		
-		// Initialize
 		id = other.id;
 		sponsor = other.sponsor;
 		title = other.title;
@@ -69,216 +54,158 @@ public class Spot {
 		description = other.description;
 	}
 	
-	
-	public Spot(ResultSet resultSet)
-	            throws SQLException {
-		
-		// Initialize
-		id = resultSet.getInt("id");
-		description = resultSet.getString("description");
-		filename = resultSet.getString("filename");
-		title = resultSet.getString("title");
-		sponsor = resultSet.getString("sponsor");
-		year = resultSet.getInt("year");
+	public Spot(ResultSet rs) throws SQLException {
+		id = rs.getInt("id");
+		description = rs.getString("description");
+		filename = rs.getString("filename");
+		title = rs.getString("title");
+		sponsor = rs.getString("sponsor");
+		year = rs.getInt("year");
 	}
 	
-	
 	/**
-	 * Creates a new %Spot from its ID in the database.
+	 * Creates a new Spot from its ID in the database.
 	 */
-	public Spot(int id) 
-	            throws SQLException {
+	public Spot(int id)  throws SQLException {
 		
-		ResultSet results;
+		ResultSet rs;
 		
 		// Execute query
 		selectStatement.setInt(1, id);
-		results = selectStatement.executeQuery();
+		rs = selectStatement.executeQuery();
 		
 		// Process results
-		results.next();
-		this.id = results.getInt("id");
-		this.sponsor = results.getString("sponsor");
-		this.title = results.getString("title");
-		this.filename = results.getString("filename");
-		this.description = results.getString("description");
-		this.year = results.getInt("year");
+		rs.next();
+		this.id = rs.getInt("id");
+		this.sponsor = rs.getString("sponsor");
+		this.title = rs.getString("title");
+		this.filename = rs.getString("filename");
+		this.description = rs.getString("description");
+		this.year = rs.getInt("year");
 	}
 	
-	
-	public static void delete(int id)
-	                          throws SQLException {
-		
-		// Execute update
+	public static void delete(int id) throws SQLException {
 		deleteStatement.setInt(1, id);
 		deleteStatement.executeUpdate();
 	}
 	
-	
-	public String getDescription() {
-		
-		return description;
+   public void insert() throws SQLException {
+      insertStatement.setString(1, sponsor);
+      insertStatement.setString(2, title);
+      insertStatement.setInt(3, year);
+      insertStatement.setString(4, filename);
+      insertStatement.setString(5, description);
+      insertStatement.executeUpdate();
+   }
+   
+   public void print() {
+      System.out.println(toString());
+   }
+   
+   public static void update(Spot original,
+                             Spot updated)
+                             throws SQLException {
+      updateStatement.setString(1, updated.sponsor);
+      updateStatement.setString(2, updated.title);
+      updateStatement.setInt(3, updated.year);
+      updateStatement.setString(4, updated.filename);
+      updateStatement.setString(5, updated.description);
+      updateStatement.setInt(6, original.id);
+      updateStatement.executeUpdate();
+   }
+   
+   //------------------------------------------------------------
+   // Helpers
+   //
+   
+	private static PreparedStatement makeDeleteStatement() throws SQLException {
+		return Database.prepare(
+		      "DELETE " +
+		      "FROM spot " +
+		      "WHERE id=?");
 	}
 	
-	
-	public String getFilename() {
-		
-		return filename;
+	private static PreparedStatement makeInsertStatement() throws SQLException {
+		return Database.prepare(
+		      "INSERT INTO spot(sponsor, title, year, filename, description) " +
+            "VALUES(?, ?, ?, ?, ?)");
 	}
 	
-	
-	public int getId() {
-		
-		return id;
+	private static PreparedStatement makeSelectStatement() throws SQLException {
+		return Database.prepare(
+		      "SELECT * " +
+		      "FROM spot " +
+		      "WHERE id = ?");
 	}
 	
-	
-	public String getSponsor() {
-		
-		return sponsor;
+	private static PreparedStatement makeUpdateStatement() throws SQLException {
+		return Database.prepare(
+		      "UPDATE spot " +
+            "SET sponsor=?, title=?, year=?, filename=?, description=? " + 
+            "WHERE id=?");
 	}
 	
+	//------------------------------------------------------------
+   // Getters and setters
+   //
 	
-	public String getTitle() {
-		
-		return title;
-	}
+   public String getDescription() {
+      return description;
+   }
+   
+   public void setDescription(String description) {
+      this.description = description;
+   }
+   
+   public String getFilename() {
+      return filename;
+   }
+   
+   public void setFilename(String filename) {
+      this.filename = filename;
+   }
+   
+   public int getId() {
+      return id;
+   }
+   
+   public String getSponsor() {
+      return sponsor;
+   }
+   
+   public void setSponsor(String sponsor) {
+      this.sponsor = sponsor;
+   }
+   
+   public String getTitle() {
+      return title;
+   }
+   
+   public void setTitle(String title) {
+      this.title = title;
+   }
+   
+   public int getYear() {
+      return year;
+   }
+   
+   public void setYear(int year) {
+      this.year = year;
+   }
 	
-	
-	public int getYear() {
-		
-		return year;
-	}
-	
-	
-	/**
-	 * Initializes the sql statement for delete a spot to the database.
-	 */
-	private static void initDeleteStatement()
-	                                        throws SQLException {
-		
-		String sql;
-		
-		sql = "DELETE FROM spot WHERE id=?";
-		deleteStatement = Database.prepare(sql);
-	}
-	
-	
-	/**
-	 * Initializes the sql statement for adding a spot to the database.
-	 */
-	private static void initInsertStatement()
-	                                        throws SQLException {
-		
-		String sql;
-		
-		sql = "INSERT INTO spot(sponsor, title, year, filename, description) " +
-		      "VALUES(?, ?, ?, ?, ?)";
-		insertStatement = Database.prepare(sql);
-	}
-	
-	
-	/**
-	 * Initializes the select statement used to retrieve a spot.
-	 */
-	private static void initSelectStatement()
-	                                        throws SQLException {
-		
-		String sql;
-		
-		sql = "SELECT * FROM spot WHERE id = ?";
-		selectStatement = Database.prepare(sql);
-	}
-	
-	
-	/**
-	 * Initializes the SQL statement used to update a spot.
-	 */
-	private static void initUpdateStatement()
-	                                        throws SQLException {
-		
-		String sql;
-		
-		sql = "UPDATE spot " +
-		      "SET sponsor=?, title=?, year=?, filename=?, description=? " + 
-		      "WHERE id=?";
-		updateStatement = Database.prepare(sql);
-	}
-	
-	
-	public void insert()
-	                   throws SQLException {
-		
-		// Execute statement
-		insertStatement.setString(1, sponsor);
-		insertStatement.setString(2, title);
-		insertStatement.setInt(3, year);
-		insertStatement.setString(4, filename);
-		insertStatement.setString(5, description);
-		insertStatement.executeUpdate();
-	}
-	
-	
-	public void print() {
-		
-		System.out.println(toString());
-	}
-	
-	
-	public void setDescription(String description) {
-		
-		this.description = description;
-	}
-	
-	
-	public void setFilename(String filename) {
-		
-		this.filename = filename;
-	}
-	
-	
-	public void setSponsor(String sponsor) {
-		
-		this.sponsor = sponsor;
-	}
-	
-	
-	public void setTitle(String title) {
-		
-		this.title = title;
-	}
-	
-	
-	public void setYear(int year) {
-		
-		this.year = year;
-	}
-	
+	//------------------------------------------------------------
+   // Converters
+   //
 	
 	public String toString() {
-		
 		return String.format("\"%s\", %d, \"%s\", \"%s\"",
 		                     sponsor, year, title, filename);
 	}
 	
+	//------------------------------------------------------------
+   // Main
+   //
 	
-	public static void update(Spot original,
-	                          Spot updated)
-	                          throws SQLException {
-		
-		updateStatement.setString(1, updated.sponsor);
-		updateStatement.setString(2, updated.title);
-		updateStatement.setInt(3, updated.year);
-		updateStatement.setString(4, updated.filename);
-		updateStatement.setString(5, updated.description);
-		updateStatement.setInt(6, original.id);
-		updateStatement.executeUpdate();
-	}
-	
-	
-	/**
-	 * Tests %Spot.
-	 */
 	public static void main(String[] args) {
 		
 		Spot spot;
